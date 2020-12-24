fd48bbcc6d61982bb95178d730f56b2d992fc34d
nipu <tokhandaker@outlook.com>
2020-12-24 16:33:20 +0900

    [REFAC] commit

--- pandas10min.py
+++ pandas10min.py
@@ -11,16 +11,29 @@ df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list("ABCD"))
 
 # print(df)
 
-df2 = pd.DataFrame( 
+df2 = pd.DataFrame(
     {
-         "A": 1.0,
-         "B": pd.Timestamp("20130102"),
-         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
-         "D": np.array([3] * 4, dtype="int32"),
-         "E": pd.Categorical(["test", "train", "test", "train"]),
-         "F": "foo",
-     }
+        "A": 1.0,
+        "B": pd.Timestamp("20130102"),
+        "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+        "D": np.array([3] * 4, dtype="int32"),
+        "E": pd.Categorical(["test", "train", "test", "train"]),
+        "F": "foo",
+    }
 )
 
-print(df2.head())
+# print(df.columns)
+# print(df.to_numpy())
+# rnd = np.random.randn(3, 2)
+# print(rnd)
 
+# dfs = df.sort_values(by="B")
+# print(dfs)
+# print(dfs["A"])
+# print(dfs[0:3])
+
+# Selection by labe
+print(df.loc[dates[1]])
+
+# Selecting on a multi-axis by label:
+print(df.loc[:,["A","B"]])
